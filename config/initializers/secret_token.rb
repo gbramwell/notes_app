d1c2488971b27e2b9627a5c38b8536f1c52bc574
Gary Bramwell <garybramwell@gmail.com>
2012-08-14 21:14:14 -0500

    Initial commit (with gems)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotesApp::Application.config.secret_token = 'd9b4de0af0abf7545a40f6bafb170a4047e9c568faee2712c8c68746436a46b43bc15fe1225ca4ccca4b231d33d0f77d0e1b9b37f74042e2363773b5b7a495d4'
